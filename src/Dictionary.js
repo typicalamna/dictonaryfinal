9519ef272a48579ebf4f280a488b33e730c89fb9
typicalamna <typicalamna@gmail.com>
2021-05-16 13:32:50 -0400

    update dictonary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
